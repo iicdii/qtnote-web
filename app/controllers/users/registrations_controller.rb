49696fa93cfb49d2fa32ebf432122d18c88f781c
Kim, Harim <iicdii@naver.com>
2016-08-16 15:49:44 +0900

    fix syntax error in registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,23 +10,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   
   def create
-  build_resource(sign_up_params)
-  pray = params[:pray] || ""
-  session[:post] = params[:whois] && params[:lesson] ? [params[:whois], params[:lesson], pray] : nil
-  if resource.save
-    yield resource if block_given?
-    if resource.active_for_authentication?
-      set_flash_message :success, :signed_up if is_flashing_format?
-      sign_up(resource_name, resource)
-      respond_with resource, location: after_sign_up_path_for(resource)
+    build_resource(sign_up_params)
+    pray = params[:pray] || ""
+    session[:post] = params[:whois] && params[:lesson] ? [params[:whois], params[:lesson], pray] : nil
+    if resource.save
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        set_flash_message :success, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message :warning, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
     else
-      set_flash_message :warning, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-      expire_data_after_sign_in!
-      respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      clean_up_passwords resource
+      respond_with resource
     end
-  else
-    clean_up_passwords resource
-    respond_with resource
   end
 
   # GET /resource/edit
