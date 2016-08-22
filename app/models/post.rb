8d6a3b1c64bb464cad9a3fd7082e054e3d18c054
Kim, Harim <iicdii@naver.com>
2016-08-23 01:01:59 +0900

    delete apply validation temporaily

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ActiveRecord::Base
     belongs_to :user
     after_create :update_streak
     
-    
     validates :whois,
         presence: { message: "내용을 입력해 주세요." },
         length: {
@@ -16,14 +15,6 @@ class Post < ActiveRecord::Base
         length: {
             minimum: 10,
             maximum: 1000,
-            too_short: "%{count}자 이상 입력해 주세요.",
-            too_long: "%{count}자 까지만 입력할 수 있습니다."
-        }
-    validates :apply,
-        presence: { message: "내용을 입력해 주세요." },
-        length: {
-            minimum: 5,
-            maximum: 300,
             too_short: "%{count}자 이상 입력해 주세요.",
             too_long: "%{count}자 까지만 입력할 수 있습니다."
         }
--- db/migrate/20160822134927_add_apply_to_posts.rb
+++ db/migrate/20160822134927_add_apply_to_posts.rb
@@ -1,8 +1,5 @@
 class AddApplyToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :apply, :string
-    Post.all.each do |post|
-      post.update_attributes!(:apply => '')
-    end
   end
 end
