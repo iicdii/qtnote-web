f45f02c14d437a52d809a166352a7b318d25fb71
Kim, Harim <iicdii@naver.com>
2016-08-23 01:29:54 +0900

    update to get max_streak_days

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       # 연속 QT일 계산
       @streak_days = current_user.streak_start && current_user.streak_end && current_user.streak_end > Time.zone.yesterday.beginning_of_day ? current_user.streak_end.day - current_user.streak_start.day : 0
-      if @streak_days >= self.user.max_streak_days
+      if @streak_days >= current_user.max_streak_days
         current_user.max_streak_days = @streak_days if @streak_days >= self.user.max_streak_days
         current_user.save
       end
