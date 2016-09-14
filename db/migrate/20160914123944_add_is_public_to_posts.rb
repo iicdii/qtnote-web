5e3a7d1a6f60ece1e65b39fbd1c91243e544dbbd
Kim, Harim <iicdii@naver.com>
2016-09-14 23:43:24 +0900

    update migration

--- db/migrate/20160914123944_add_is_public_to_posts.rb
+++ db/migrate/20160914123944_add_is_public_to_posts.rb
@@ -1,8 +1,5 @@
 class AddIsPublicToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :is_public, :boolean, default: false
-    Post.all.each do |post|
-      post.update_attributes!(:is_public => false)
-    end
   end
 end
